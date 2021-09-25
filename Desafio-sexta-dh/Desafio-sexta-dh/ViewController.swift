7281cb284a0f21417f6df386c48e5632ede66c46
Magno Miranda Dantas <magnodantas1991@gmail.com>
2021-09-24 22:13:46 -0300

    Finalizando o projeto

--- Desafio-sexta-dh/Desafio-sexta-dh/ViewController.swift
+++ Desafio-sexta-dh/Desafio-sexta-dh/ViewController.swift
@@ -14,12 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var profession1: UILabel!
     @IBOutlet weak var profession2: UILabel!
     
+    var pessoas: [Pessoa] = [.init(nome: "João", idade: 25, cargo: "Advogado", salario: 5000.0), .init(nome: "Maria", idade: 23, cargo: "Nutricionista", salario: 5000.0), .init(nome: "Francisco", idade: 24, cargo: "Dev iOS", salario: 5000.0), .init(nome: "Fernando", idade: 20, cargo: "Dev Android", salario: 5000.0)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Draw()
     }
     
     @IBAction func drawNames(_ sender: Any) {
+        Draw()
+    }
+    
+    func Draw() {
+        let person1 = pessoas[Int(arc4random_uniform(UInt32(pessoas.count)))]
+        let person2 = pessoas[Int(arc4random_uniform(UInt32(pessoas.count)))]
+        
+        fillLabels(person1: person1, person2: person2)
+    }
+    
+    func fillLabels(person1: Pessoa, person2: Pessoa) {
+        if person1.nome == person2.nome {
+            Draw()
+        }
+        
+        name1.text = person1.nome
+        name2.text = person2.nome
+        profession1.text = person1.cargo
+        profession2.text = person2.cargo
     }
     
 
