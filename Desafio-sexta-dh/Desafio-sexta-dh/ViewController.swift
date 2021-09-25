d8e90e003682796ba9046a6274e95831b1f12997
Magno Miranda Dantas <magnodantas1991@gmail.com>
2021-09-24 21:43:39 -0300

    Revert "Models"

--- Desafio-sexta-dh/Desafio-sexta-dh/ViewController.swift
+++ Desafio-sexta-dh/Desafio-sexta-dh/ViewController.swift
@@ -8,20 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var name1: UILabel!
-    @IBOutlet weak var name2: UILabel!
-    @IBOutlet weak var profession1: UILabel!
-    @IBOutlet weak var profession2: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func drawNames(_ sender: Any) {
-    }
-    
+
 
 }
 
